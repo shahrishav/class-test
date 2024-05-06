e6de21972e76707f98558b348040b3b83c2d0927
shahrishav <shahrishav11@gmail.com>
2024-05-06 13:12:49 +0545

    Add initial project files and update screens

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'grid_view_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const GridViewScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Welcome Rishav Sah",
+          style: TextStyle(fontSize: 24),
+      ),
+      ),
+    );
+  }
+}
